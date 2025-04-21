b53fcfb86a98c553bee832793fff1caf91670f8a
oussamasine07 <sineoussama@gmail.com>
2025-04-21 22:58:13 +0100

    make header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    NavbarComponent, FooterComponent
+    NavbarComponent, FooterComponent, HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
