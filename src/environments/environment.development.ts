a1e5904eae955ea6603c98532f8832c6ef46f752
oussamasine07 <sineoussama@gmail.com>
2025-04-23 12:48:23 +0100

    add firbase config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBbkNrVi6Z5XCPS2zUU-ve3Ka_TydYAXdk",
+        authDomain: "enna-blogger.firebaseapp.com",
+        projectId: "enna-blogger",
+        storageBucket: "enna-blogger.firebasestorage.app",
+        messagingSenderId: "220538499802",
+        appId: "1:220538499802:web:dcb41c7dabf905681dabe3",
+        measurementId: "G-RRBC1T3691"
+    }
+};
