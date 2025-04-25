5740e3263d27b41ff91f822bbb177ec16f0f8539
krrsalmane <karrounsalmane@gmail.com>
2025-04-25 10:32:02 +0100

    finish show single artical functionality

--- src/app/components/pages/single-artical/single-artical.component.ts
+++ src/app/components/pages/single-artical/single-artical.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ArticalsService } from '../../../services/articals/articals.service';
 
 @Component({
   selector: 'app-single-artical',
@@ -8,13 +9,24 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SingleArticalComponent implements OnInit {
   @Input() artical!: {
+    id: string;
     title: string, 
     articalBody: string, 
     image: string
   }
+
+  articalService = inject(ArticalsService)
   
   ngOnInit(): void {
-    
+    this.articalService.getArticalById("3dNAKIkBEtVSEbeI3cQ8").subscribe({
+      next: (artical) => {
+        console.log(artical)
+        this.artical = artical
+      },
+      error: (err) => {
+        console.log(err.message)
+      }
+    })
   }
 
 }
--- src/app/services/articals/articals.service.ts
+++ src/app/services/articals/articals.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { collection, collectionData, doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { from, Observable } from 'rxjs';
 import { ArticalInterface } from '../../interfaces/artical-interface';
 
 @Injectable({
@@ -13,12 +13,33 @@ export class ArticalsService {
   fireStore = inject(Firestore)
   articalsCollection = collection(this.fireStore, "articales");
 
+  // get all articals
   getArtical (): Observable<ArticalInterface[]> {
     return collectionData(this.articalsCollection, {
       idField: "id"
     }) as Observable<ArticalInterface[]>;
   }
 
+  // get artical by id
+  getArticalById (id: string): Observable<ArticalInterface> {
+    const docRef = doc(this.fireStore, `articales/${id}`)
+    const promise = getDoc(docRef).then(res => {
+      console.log(res.exists())
+      if (res.exists()) {
+        return {
+          id: res.id, ...res.data() as any
+        }
+      } else {
+        return null
+      }
+    });
+
+    return from(promise)
+
+  }
+
   
 
 }
+
+
